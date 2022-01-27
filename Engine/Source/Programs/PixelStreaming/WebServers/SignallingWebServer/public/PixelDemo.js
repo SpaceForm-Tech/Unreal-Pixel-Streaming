f56b0e9628adef849d1b905ec9a10c8e0166883a
Dan Pitts <dan.pitts@squintopera.com>
2022-01-27 17:20:09 +0000

    Turning fake mouse with touch back on

--- Engine/Source/Programs/PixelStreaming/WebServers/SignallingWebServer/public/PixelDemo.js
+++ Engine/Source/Programs/PixelStreaming/WebServers/SignallingWebServer/public/PixelDemo.js
@@ -162,7 +162,7 @@ function onParagonLoad() {
 	afk.closeTimeout = 10;
 	styleAdditional = grabStyle;
 	inputOptions.controlScheme = ControlSchemeType.HoveringMouse;
-	inputOptions.fakeMouseWithTouches = false;
+	inputOptions.fakeMouseWithTouches = true;
 	styleWidth = 700;
 	styleHeight = 394;
 
